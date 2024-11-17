ada452494b6652f3cdad84e95b894f72646ccb94
marcelogmoura <mgmoura@gmail.com>
2024-11-17 10:47:16 -0300

    ngx pagination e spinner adicionados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './components/layout/menu/menu.component';
+import { NgxSpinner, NgxSpinnerModule } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    MenuComponent
+    MenuComponent,
+    NgxSpinnerModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/components/pages/consulta-produtos/consulta-produtos.component.ts
+++ src/app/components/pages/consulta-produtos/consulta-produtos.component.ts
@@ -3,20 +3,27 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { RouterLink } from '@angular/router';
+import { NgxPaginationModule } from 'ngx-pagination';
+
+
 
 @Component({
   selector: 'app-consulta-produtos',
   standalone: true,
   imports: [
     CommonModule,
-    RouterLink
-  ],
+    RouterLink,
+    NgxPaginationModule
+    
+    
+],
   templateUrl: './consulta-produtos.component.html',
   styleUrl: './consulta-produtos.component.css'
 })
 export class ConsultaProdutosComponent implements OnInit{
 
   produtos: any[] = [];
+  pagina: number = 1;
 
   constructor(
     private httpClient: HttpClient
@@ -55,4 +62,8 @@ export class ConsultaProdutosComponent implements OnInit{
         
     }
   }
+
+  handlePageChange(event: any) {
+    this.pagina = event;
+    }
 }
